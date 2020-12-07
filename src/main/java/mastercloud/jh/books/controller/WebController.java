49f8a3ef8f74de2a1011e99ac735d58ec8f4cc2c
jaime.hernandez <jaime.hernandez@bi-geek.com>
2020-12-07 01:08:19 +0100

    Added templates.

--- src/main/java/mastercloud/jh/books/controller/WebController.java
+++ src/main/java/mastercloud/jh/books/controller/WebController.java
@@ -6,6 +6,7 @@ import mastercloud.jh.books.service.UserSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class WebController {
@@ -21,9 +22,22 @@ public class WebController {
     }
 
     @GetMapping("/web/books")
-    public String getIndex(Model model){
+    public String getBooks(Model model){
         model.addAttribute("books", this.bookService.getBooks());
         model.addAttribute("userName", userSession.getUser());
         return "initial_template";
+    }
+
+    @GetMapping("/web/books/{bookId}")
+    public String getBook(Model model, @PathVariable("bookId") Long bookId){
+        model.addAttribute("userName", userSession.getUser());
+        model.addAttribute("book", this.bookService.getBook(bookId));
+        return "particular_book_template";
+    }
+
+    @GetMapping("web/books/form")
+    public String openCreateBookWindow(Model model){
+        model.addAttribute("userName", userSession.getUser());
+        return "newBook_template";
     }
 }
